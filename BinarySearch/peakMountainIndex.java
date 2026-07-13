ceac5d049871cc4a1309c3fef6209aa725acc707
Ashraf Alam <ashrafalam.tech@gmail.com>
2026-07-13 16:43:24 +0530

    solved mountain peak problem

---
+++ BinarySearch/peakMountainIndex.java
@@ -0,0 +1,20 @@
+package BinarySearch;
+
+public class peakMountainIndex {
+    public static int peakIndexInMountainArray(int[] arr) {
+        int n = arr.length;
+        int low = 1, high = n - 2;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] > arr[mid -1] && arr[mid]> arr[mid+1]) return mid;
+            else if(arr[mid] > arr[mid -1] && arr[mid] < arr[mid+1]) low = mid + 1;
+            else if(arr[mid] < arr[mid -1] && arr[mid] >arr[mid+1]) high = mid - 1;
+
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,4,3,2,10};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+}
