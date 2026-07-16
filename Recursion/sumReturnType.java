9cafeaee8a238a12e58a57c731bf8a71e9efb24d
Ashraf Alam <ashrafalam.tech@gmail.com>
2026-07-16 22:29:06 +0530

    solved sum of n number using recursion , with the help of return type

---
+++ Recursion/sumReturnType.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class sumReturnType {
+    public static int sum(int n){
+        if(n==0 || n==1) return n;
+        return n+sum(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        System.out.println(sum(n));
+        sc.close();
+    }
+}
