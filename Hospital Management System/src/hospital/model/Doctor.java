b86c75b3deedb15ba121b4a21ccf564555b4cf04
Ashraf Alam <ashrafalam.tech@gmail.com>
2026-07-21 08:28:37 +0530

    Add doctor model

---
+++ Hospital Management System/src/hospital/model/Doctor.java
@@ -0,0 +1,23 @@
+package hospital.model;
+
+public class Doctor {
+    private static int idCounter = 1;
+    private int id;
+    private String name;
+    private String speciality;
+
+    public Doctor(String name, String speciality){
+
+        this.id = idCounter++;
+        this.name = name;
+        this.speciality = speciality;
+    }
+
+    public int getId(){
+        return id;
+    }
+    @Override
+    public String toString(){
+        return "Patient ID: " + id + ", Name: " + name + ", Speciality: " + speciality; 
+    }
+}
