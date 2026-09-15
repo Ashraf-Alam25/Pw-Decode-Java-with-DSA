b4beafa26dfe3bd98173383fee6a8fdfaa1cc428
Ashraf Alam <ashrafalam.tech@gmail.com>
2026-09-16 03:42:58 +0530

    solved matrix chain multiplicationn problem with dp

---
+++ DynamicPrograming/MatrixChainMultiplication.java
@@ -0,0 +1,38 @@
+public class MatrixChainMultiplication{
+    /*
+    this method is for 2D matrix
+     */
+    private static int mcm(int i, int j, int[][] arr) {
+        if(i==j) return 0;
+        int minCost = Integer.MAX_VALUE;
+        for (int k = i; k < j; k++) {
+            int x = arr[i][0] * arr[k][1] * arr[j][1] ;
+            int totalCost = mcm(i,k,arr) + mcm(k+1, j, arr) + x;
+            minCost = Math.min(minCost, totalCost);
+        }
+        return  minCost;
+    }
+
+    
+    /*
+    this one metod is for 1 D matrix
+     */
+    private static int mcm(int i, int j, int[] arr) {
+        if(i==j) return 0;
+        int minCost = Integer.MAX_VALUE;
+        for (int k = i; k < j; k++) {
+            int x = arr[i] * arr[k+1] * arr[j+1];
+            int totalCost = mcm(i,k,arr) + mcm(k+1, j, arr) + x;
+            minCost = Math.min(minCost, totalCost);
+        }
+        return  minCost;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4};
+        int n = arr.length;
+        System.out.println(mcm(0,n-2,arr));
+
+    }
+
+}
---
+++ DynamicPrograming/MatrixChainMultiplicationTabulation.java
@@ -0,0 +1,26 @@
+public class MatrixChainMultiplicationTabulation {
+    private static  int mcm(int arr[]){
+        int n = arr.length;
+        int dp[][] = new int[n-1][n-1];
+        for(int i = n-2; i >= 0; i--){
+            for(int j = 0; j <n-1; j++){
+                if(i >= j){
+                    dp[i][j] = 0;
+                    continue;
+                }
+                int minCost = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    int x = arr[i]* arr[k+1]* arr[j+1];
+                    int totalCost = dp[i][k] + dp[k+1][j]+x;
+                    minCost = Math.min(minCost, totalCost);
+                }
+                dp[i][j] = minCost;
+            }
+        }
+        return dp[0][n-2];
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4};
+        System.out.println(mcm(arr));
+    }
+}
